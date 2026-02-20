2a5f61cbeaa917b270ac1415a35936f021364f07
Baquiran-9e-ay2526 <baquiran.hm@obmontessori.edu.ph>
2026-02-20 11:27:02 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function buttonDiscount (){
+    let origBudget = document.getElementById(budgetval).value;
+    let discountedPrice = Number(origBudget * 0.2); 
+
+    document.getElementById().innerHTML = '
+        <div class="container">
+    
+    </div>
+}
